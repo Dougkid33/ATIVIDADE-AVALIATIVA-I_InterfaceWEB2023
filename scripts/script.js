fd4931f2b9112210f2a33921327f7369dd1cc85f
Dougkid33 <douglasaugustomsilva@hotmail.com>
2023-03-11 19:18:28 -0300

    primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+$(function() {
+    // Configuração dos datepickers
+    $("#datepicker").datepicker();
+    $("#datepickerreturn").datepicker();
+    
+    // Ação do botão enviar
+    $( ".widget input[type=submit], .widget a, .widget button" ).button();
+    $("input[type='submit']").click(function(event) {
+      event.preventDefault(); // previne o envio do formulário
+      
+      // Verifica se todos os campos foram preenchidos
+      if ($("#valordiaria input").val() == "" || $("#datepicker").val() == "" || $("#datepickerreturn").val() == "") {
+        alert("Preencha todos os campos.");
+        return;
+      }
+      
+      // Verifica se a data de retorno é maior ou igual à data de ida
+      var dataida = new Date($("#datepicker").val());
+      var dataretorno = new Date($("#datepickerreturn").val());
+      if (dataretorno < dataida) {
+        $("#modal").text("A data de retorno não pode ser superior à de ida");
+        $("#modal").dialog();
+        return;
+      }
+      
+      // Verifica se a diferença entre as datas é maior que 30 dias
+      var diferenca = (dataretorno - dataida) / (1000 * 60 * 60 * 24); // converte para dias
+      if (diferenca > 30) {
+        $("#modal").text("A diferença entre as datas não pode ser superior a 30 dias");
+        $("#modal").dialog();
+        return;
+      }
+      
+      // Calcula o valor total
+      var valordiaria = parseFloat($("#valordiaria input").val());
+      var totaldiarias = diferenca == 0 ? 0.5 : diferenca - 1;
+      var valortotal = totaldiarias * valordiaria + valordiaria / 2;
+      
+      // Exibe o resultado no modal
+      $("#modal").html("<p>O valor total é: R$ " + valortotal.toFixed(2) + "</p>");
+      $("#modal").dialog();
+      console.log(valortotal);
+    });
+  });
+  
